842d1744552e4e69b6d4aaa9572a5a552c311aec
imanuel <toswim100@gmail.com>
2024-06-07 02:11:22 +0300

    add some card of list of menus

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -2,23 +2,28 @@ import React, { FC } from "react";
 import "./Card.css";
 import ButtonX from "../buttonCustom/ButtonX";
 import AddCircleIcon from "@mui/icons-material/AddCircle";
-
 interface props {
   title: string;
   description: string;
   price: number;
+  image?: string;
 }
 
-const Card: FC<props> = ({ title= "אריזוטו", description="כהגה הקה קהק", price=12 }) => {
+const Card: FC<props> = ({
+  title = "אריזוטו",
+  description = "כהגה הקה קהק",
+  price = 12,
+  image = "",
+}) => {
   return (
     <div className="c-card-container">
-      <div className="background-container">
-        <ButtonX Icon={{ isButton: true, IconName: AddCircleIcon }} />
-      </div>
       <div className="details-container">
         <span className="card-title">{title}</span>
         <span className="card-description">{description}</span>
         <span className="card-price">{`${price} ₪`}</span>
+      </div>
+      <div className="background-container">
+        <ButtonX Icon={{ isButton: false, IconName: AddCircleIcon }} />
       </div>
     </div>
   );
--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,26 +1,53 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import "./Main.css";
+import Card from "../../components/card/Card";
 
 const Main = ({
   dishType,
 }: {
   dishType: { image: string; name: string }[];
 }) => {
+  const [selectType, setSelectType] = useState<keyof typeof list>();
+
+  const list = {
+    ראשונות: [
+      { title: "string", description: "string", price: 33 },
+      { title: "string11", description: "string111", price: 331 },
+    ],
+  };
+
   return (
     <div className="c-main-page">
-      {dishType.map((type, index) => (
-        <div
-          key={index}
-          className="c-card-container"
-          style={{
-            backgroundImage: `url(${type.image})`,
-          }}
-        >
-          <div className="card-dark-background">
-            <span>{type.name}</span>
+      {selectType ? (
+        <div>
+          <span className="type-title">{selectType}</span>
+          <div className="list-container">
+            {list[selectType].map(({ title, description, price }, index) => (
+              <Card
+                key={index}
+                title={title}
+                description={description}
+                price={price}
+              />
+            ))}
           </div>
         </div>
-      ))}
+      ) : (
+        dishType.map((type, index) => (
+          <div
+            key={index}
+            className="c-card-container"
+            style={{
+              backgroundImage: `url(${type.image})`,
+            }}
+            onClick={() => setSelectType(type.name as keyof typeof list)}
+          >
+            <div className="card-dark-background">
+              <span>{type.name}</span>
+            </div>
+          </div>
+        ))
+      )}
     </div>
   );
 };
