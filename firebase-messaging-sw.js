e0218acaa04676d3ec5ed0a1b9ce1dbe0ebce4a0
imanuel <toswim100@gmail.com>
2024-07-11 16:21:55 +0300

    Updates

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -16,15 +16,15 @@ firebase.initializeApp({
 const messaging = firebase.messaging();
 
 // Customize notification handler here
-messaging.onBackgroundMessage(function (payload) {
-    console.log('Received background message ', payload);
-    // Customize notification here
-    const notificationTitle = 'Background Message Title';
-    const notificationOptions = {
-        body: 'Background Message body.',
-        icon: '/firebase-logo.png'
-    };
+// messaging.onBackgroundMessage(function (payload) {
+//     console.log('Received background message ', payload);
+//     // Customize notification here
+//     const notificationTitle = 'Background Message Title';
+//     const notificationOptions = {
+//         body: 'Background Message body.',
+//         icon: '/firebase-logo.png'
+//     };
 
-    self.registration.showNotification(notificationTitle,
-        notificationOptions);
-});
+//     self.registration.showNotification(notificationTitle,
+//         notificationOptions);
+// });
